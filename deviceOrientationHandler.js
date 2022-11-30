c41144445984d3d229c7e91c8793dc10b3ea318b
YK Chong <chongyongkhong.yk@gmail.com>
2022-12-01 01:38:20 +0800

    Added landscape mode canvas

---
+++ deviceOrientationHandler.js
@@ -0,0 +1,19 @@
+function updateRotateDeviceCanvas(display) {
+    document.getElementById('treasure-hunt-landscape-overlay').style.display = display ? 'flex' : 'none';
+    document.getElementById('treasure-hunt-content').style.display = display ? 'none' : 'block';
+}
+function appendRotateDeviceCanvas() {
+    var str = '<link rel="stylesheet" href="./style.css" /><div id="treasure-hunt-landscape-overlay" style="display: flex"><p>Please rotate your phone to Potrait Mode</p><img src="./images/RotatePhone.png" /></div>';
+    document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", str);
+}
+
+appendRotateDeviceCanvas();
+
+let matchMediaQuery = "(orientation: landscape)";
+let landscape = window.matchMedia(matchMediaQuery);
+
+updateRotateDeviceCanvas(landscape.matches);
+
+landscape.addEventListener("change", function (e) {
+    updateRotateDeviceCanvas(e.matches);
+});
